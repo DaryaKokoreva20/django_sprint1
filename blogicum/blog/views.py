0ee466cd6ae04dd6a2940411b589c0257ee2cd1f
Daria Kokoreva <kokorewa.dashenka@yandex.ru>
2024-10-25 16:23:39 +0300

    Delete extra imports

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -58,7 +58,8 @@ def post_detail(request, id):
 
 def category_posts(request, category_slug):
     template = 'blog/category_posts.html'
-    filtered_posts = [post for post in posts if post['category'] == category_slug]
+    filtered_posts = [
+        post for post in posts if post['category'] == category_slug
+    ]
     context = {'posts': filtered_posts}
     return render(request, template, context)
-
